b1cf7459f6dd6daf77abcb8f5ab4166843a86175
orzoon <orzoonkunwar7.ak@gmail.com>
2020-04-10 23:59:22 +1000

    changes

---
+++ src/Components/Dashboard/LineChart.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function LineChart(props){
+
+
+    return (
+        <div className = "DLineChart_container">
+            
+        </div>
+    )
+}
--- src/Components/MidSection/Dashboard.js
+++ src/Components/MidSection/Dashboard.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+
+/* D_Components Imports */
+import DBanner from "../Dashboard/DBanner";
+import LineChart from "../Dashboard/LineChart"
+// scss style
+import "../../css/Dashboard.scss";
+
 export default function Dashboard(props){
     return (
         <div className = "dashboardContainer">
-            dashboard
+           <DBanner />
+           <LineChart />
         </div>
     )
 }
